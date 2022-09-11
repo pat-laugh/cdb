9220705bd72fbffe8f4f5939d903b35f11efc02f
Patrick Laughrea <patlaughweb@gmail.com>
2022-09-11 18:07:34 -0400

    Improve and refactor install script

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 run_apt_get_install='sudo apt-get install g++ make libboost-all-dev'
-echo 'The binaries g++ and make, and the C++ Boost libraries Filesystem and System are'
-echo 'required. The following command can install them:'
-echo
-echo "$run_apt_get_install"
-echo
+echo "\
+The binaries g++ and make, and the C++ Boost libraries Filesystem
+and System are required. The following command can install them:
+
+$run_apt_get_install
+"
 read -p 'Install them now (the command above will be executed) [y/N]? ' a
-if [ "$a" = 'y' ]; then
+if [ "$a" = y ]; then
     $run_apt_get_install
 fi
 unset a run_apt_get_install
+
+mkdir -p ~/bin
 
 # Build back end
 cur_dir=`pwd`
@@ -24,13 +27,17 @@ make clean
 # Build and source front end
 cd "$cur_dir"
 cdb_mv() {
-    local name path
+    local line name path
     name=$1
     cp $name .$name
     sed -i "s|$name|$HOME/bin/.$name|" .$name
     path="$HOME/bin/.$name.sh"
     mv .$name "$path"
-    echo ". $path" >>~/.bashrc
+    line=". $path"
+    grep -Fx "$line" ~/.bashrc 2>&1 >/dev/null
+    if [ $? != 0 ]; then
+        echo "$line" >>~/.bashrc
+    fi
 }
 cd front-end/bash-completion
 cdb_mv cdb-bc
@@ -39,3 +46,11 @@ mv cdb cdb-back
 cdb_mv cdb-back
 mv cdb-back cdb
 unset cdb_mv
+
+cd "$cur_dir"
+. ~/.bashrc
+echo "\
+To start using cdb, source the ~/.bashrc file, or open a new terminal.
+
+. ~/.bashrc
+"
